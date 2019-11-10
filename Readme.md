939eea854729ca660342e2c02d066662c7e70780
Nicole Alexander <nicole@Ians-MacBook-Air-2.local>
2019-11-09 19:53:28 -0500

    added remaining bmi statuses and created Readme.md

---
+++ Readme.md
@@ -0,0 +1,2 @@
+* bmi-calculator
+A simple python function that calculates a person's BMI from their weight and height. 
--- bmi-calculator.py
+++ bmi-calculator.py
@@ -1,6 +1,21 @@
-def bmi_calculator(age, weight, height):
+def bmi_calculator(weight, height):
     bmi_constant = 703
-    BMI = (weight * bmi_constant) / (height**2) 
-    return (BMI)
+    bmi_status = None 
+    
 
-print(bmi_calculator(25, 139, 65))
+    BMI = (weight * bmi_constant) / (height**2) 
+    
+    if BMI < 18.5:
+        bmi_status = "underweight"
+    elif BMI < 25:
+        bmi_status = "normal weight"
+    elif BMI < 30:
+        bmi_status = "overweight"
+    else: 
+        bmi_status = "obese"
+    
+    return ("The BMI for a height of " + str(height) + " inches and a weight of " + str(weight) + " pounds is " + str(BMI) + ". This person would be considered " + bmi_status + ".")
+
+print(bmi_calculator(139, 65))
+
+
